e18f343dfd86399fc08c1bbc8018dc7cba351cd8
SebastianoGuzzon <sebyguzz@icloud.com>
2023-04-18 18:22:11 +0200

    Modifica css

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ createApp({
           // Inserimento delle 5 immagini 
             counter: 0,
             image: [
-                "",
-                "",
-                "",
-                "",
-                ""
+                "https://cdn.wallpapersafari.com/27/97/vMhYDQ.png",
+                "https://images.unsplash.com/photo-1503614472-8c93d56e92ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8NGslMjBtb3VudGFpbnxlbnwwfHwwfHw%3D&w=1000&q=80",
+                "https://images.unsplash.com/photo-1549740425-5e9ed4d8cd34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwzOTU0NTB8fGVufDB8fHx8&w=1000&q=80",
+                "https://wallpapers.com/images/featured/ztbad1qj8vdjqe0p.jpg",
+                "https://images2.alphacoders.com/107/1074989.jpg"
             ],
             time: null,
             hover: false
